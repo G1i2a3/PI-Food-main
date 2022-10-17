d78a6dfb75df94632c4eb5a53a953820016321c7
Giannina <giannisedler@hotmail.com>
2022-10-17 12:54:19 -0300

    Home esta andando

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+import Home from './containers/home/home';
+
+//import { Link } from 'react-router-dom';
+
 
 function App() {
+  // const redirect = () => {
+  // }
   return (
+
     <div className="App">
-      <h1>Henry Food</h1>
+      <h1 className='App-header'>Bienvenidos!</h1>
+      <Link to="/home">HOME</Link>
     </div>
+
   );
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//       <React.Fragment>
+//           <NavBar />
+//           <Route exact path="/" component={Buscador} />
+//           <Route path="/favs" component={Favorites} />
+//           <Route path="/movie/:id" component={Movie} />
+//       </React.Fragment>
+//   );
+// }
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from "./containers/home/home"
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//     <App />
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// ); //==> venia hecho
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>
+  }
+  
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
